4722dda3567ec748d673ad90d53c92ee88f3c0a3
John Culviner <john.culviner@gmail.com>
2015-01-20 00:06:28 -0600

    added bus data scraper

--- server/worker.js
+++ server/worker.js
@@ -1,10 +1,20 @@
-var http = require('http');
-var mongo = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
+var request = require('request');
+
+var db;
+
+MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/msp-bus-live', function (err, database) {
+  if (err) throw err;
+
+  db = database;
+
+  setInterval(storeVehicleLocations, 30000);
+  storeVehicleLocations();
+});
 
 
 function storeVehicleLocations() {
-
+  request.get('http://svc.metrotransit.org/NexTrip/VehicleLocations/0?format=json', function(err, resp, body) {
+    db.collection('vehicleLocations').insert(JSON.parse(body), { w: 0 });
+  });
 }
-
-setInterval(storeVehicleLocations, 30000);
-storeVehicleLocations();
